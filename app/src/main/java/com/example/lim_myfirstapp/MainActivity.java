9ea97ca5c2015514e21ad819ba9a1b0bb6a4d023
PocariNoSweat <christianlim0031@gmail.com>
2022-01-07 17:21:51 +0900

    Super Idol

--- app/src/main/java/com/example/lim_myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/lim_myfirstapp/MainActivity.java
@@ -2,13 +2,21 @@ package com.example.lim_myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player = new MediaPlayer();
+        player= MediaPlayer.create(this,R.raw.jiyangliaoshwei);
+
+        player.start();
     }
 }
